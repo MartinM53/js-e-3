982d022ed31037fa0a4753bb12f768dff353b7ea
Martin M <martin99953@hotmail.com>
2023-08-10 21:03:14 -0300

    as

---
+++ index.js
@@ -0,0 +1,60 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    imagen: "./img/muzzarella.png",
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    imagen: "./img/cebolla.png",
+  },
+
+  {
+    id: 3,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+    imagen: "./img/4quesos.png",
+  },
+
+  {
+    id: 4,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
+    imagen: "./img/especial.png",
+  },
+
+  {
+    id: 5,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    imagen: "./img/anana.png",
+  },
+];
+
+const inputId = document.querySelector("#inputId");
+const btnId = document.querySelector("#btnId");
+const container = document.querySelector("#container");
+
+btnId.addEventListener("click", () => {
+  const selectedId = parseInt(inputId.value);
+});
+// Convierte el valor del input a un número entero
+
+// Busca la pizza con el ID seleccionado
+
+const selectedPizza = pizzas.find((pizza) => pizza.id === selectedId);
